02281172ce9281e2985c54d9302c71078b44852b
Taras Stolitnii <tariiik@MacBook-Pro-Tar4ik.local>
2025-07-30 17:20:31 +0300

    Switch DB to use DATABASE_URL if set

--- onboarding_crm/__init__.py
+++ onboarding_crm/__init__.py
@@ -2,21 +2,26 @@ from flask import Flask
 from onboarding_crm.extensions import db, login_manager
 from onboarding_crm.routes import bp
 from onboarding_crm.models import User
-from onboarding_crm.utils import register_custom_filters  # 🔄 імпортуємо
-
+from onboarding_crm.utils import register_custom_filters
 import os
 
 def create_app():
     app = Flask(__name__)
-    app.jinja_env.cache = {}  # 🔄 очищення кешу шаблонів
+    app.jinja_env.cache = {}
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, '../instance/onboarding.db')}"
+    # 📌 Если есть DATABASE_URL, используем PostgreSQL
+    db_url = os.getenv("DATABASE_URL")
+    if db_url:
+        app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    else:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, '../instance/onboarding.db')}"
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
-
     login_manager.login_view = 'login'
 
     @login_manager.user_loader
@@ -24,8 +29,6 @@ def create_app():
         return User.query.get(int(user_id))
 
     app.register_blueprint(bp)
-
-    # ✅ Реєстрація кастомного Jinja2 фільтра
     register_custom_filters(app)
 
     return app
